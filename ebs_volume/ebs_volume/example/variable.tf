c0bdd5dca5403a361cf2ee6d57bc4d88df01fea5
Kartikay769 <79659443+Kartikay769@users.noreply.github.com>
2022-03-29 02:00:49 +0530

    Add files via upload

---
+++ ebs_volume/ebs_volume/example/variable.tf
@@ -0,0 +1,14 @@
+variable "az"{
+    type = string
+    description = "this is the Availability zone for EBS volume "
+}
+
+variable "size"{
+    type = string
+    description = "This is for size of EBS "
+}
+
+variable "tag_name"{
+    type = string
+    description = "this is tag name of EBS "
+}
